5b3bba43360838b2406af16ddbc980833b9bdbc6
forgohill <forgohill@gmail.com>
2026-04-15 15:31:06 +0700

    refactor(php-interface): удалить неиспользуемый Helper и очистить init chore(gitignore): добавить .htaccess.save в игнор

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,17 +3,3 @@ $autoload = $_SERVER['DOCUMENT_ROOT'] . '/local/vendor/autoload.php';
 if (file_exists($autoload)) {
     require_once $autoload;
 }
-
-// use App\Iblock\Event\ElementEventHandler;
-
-// AddEventHandler(
-//     'iblock',
-//     'OnBeforeIBlockElementAdd',
-//     [ElementEventHandler::class, 'onBeforeElementAdd']
-// );
-
-// AddEventHandler(
-//     'iblock',
-//     'OnBeforeIBlockElementUpdate',
-//     [ElementEventHandler::class, 'onBeforeElementUpdate']
-// );

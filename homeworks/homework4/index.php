72db760e2b7ed1f17b8e384eaabcef5e8c6b268a
forgohill <forgohill@gmail.com>
2026-04-26 10:28:31 +0700

    chore(museum-stories): удалить старые файлы установки

--- homeworks/homework4/index.php
+++ homeworks/homework4/index.php
@@ -10,8 +10,5 @@ Asset::getInstance()->addCss('//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bo
 ?>
 <h1 class="mb-3"><? $APPLICATION->ShowTitle() ?></h1>
 
-<a class="btn btn-primary" href="/homeworks/homework4/install-museum-stories.php">
-  Открыть установку инфоблоков Museum Stories
-</a>
 
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>

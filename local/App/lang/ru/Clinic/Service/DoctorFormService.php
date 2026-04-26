2d234470cb2ad9afdffe57896487a89800ddfda4
forgohill <forgohill@gmail.com>
2026-04-27 00:22:16 +0700

    chore(lang): удалить неиспользуемые языковые файлы

--- local/App/lang/ru/Clinic/Service/DoctorFormService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$MESS['CLINIC_DOCTOR_FORM_SERVICE_EMPTY_LAST_NAME'] = 'Не заполнена фамилия врача.';
-$MESS['CLINIC_DOCTOR_FORM_SERVICE_EMPTY_FIRST_NAME'] = 'Не заполнено имя врача.';
-$MESS['CLINIC_DOCTOR_FORM_SERVICE_EMPTY_INN'] = 'Не заполнен ИНН врача.';
-$MESS['CLINIC_DOCTOR_FORM_SERVICE_INVALID_INN'] = 'ИНН должен состоять из 12 цифр и не может начинаться с 0.';
-$MESS['CLINIC_DOCTOR_FORM_SERVICE_INVALID_BIRTH_DATE'] = 'Некорректный формат даты рождения.';
-$MESS['CLINIC_DOCTOR_FORM_SERVICE_CREATE_ERROR'] = 'Не удалось создать врача.';
-$MESS['CLINIC_DOCTOR_FORM_SERVICE_UPDATE_ERROR'] = 'Не удалось обновить врача.';
--- local/App/lang/ru/Clinic/Service/ProcedureFormService.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$MESS['CLINIC_PROCEDURE_FORM_SERVICE_EMPTY_NAME'] = 'Не заполнено название процедуры.';
-$MESS['CLINIC_PROCEDURE_FORM_SERVICE_CREATE_ERROR'] = 'Не удалось создать процедуру.';
